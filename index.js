8ebe85af0b6dc7c55efe77ed89d22bf251f2831a
patricklee100 <patricklee100@gmail.com>
2016-12-31 10:33:15 -0400

    final

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 
 var app = express();
+var count = 0;
 
 app.set("port", process.env.PORT || 3000);
 app.get('/', function(req, res) {
-   res.send("<h1>Patrick Lee, Hello World!</h1>");
+   res.send("<h1>Patrick Lee, Hello World!</h1>" + count++);
 });
 
 app.listen(app.get("port"), function() {
